b5085513d2354166cd257ee32c805f7bc9c4889f
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2021-05-21 21:29:16 -0400

    themed calculator

--- dist/index.js
+++ dist/index.js
@@ -1,122 +1,139 @@
-//To get Slider Value
-function getThemeVal() {
-  setTheme(3)
-}
-
 var slider = document.getElementById('sliderValue')
 var value = slider.value
+
 slider.oninput = function () {
   value = this.value
   setTheme(value)
 }
 
 function setTheme(val) {
+  var button = document.querySelectorAll('.btn')
   if (value == 1) {
     document.body.style.backgroundColor = 'hsl(222, 26%, 31%)'
     document.querySelector('.calculator__header').style.color = 'white'
     document.querySelector('.theme__title ').style.color = 'white'
-    document.querySelector('.slider').style.background = 'hsl(223, 31%, 20%)'
-    document.getElementsByClassName('screen')[0].style.background =
-      'hsl(224, 36%, 15%)'
+    document.querySelector('.slider').style.background = '#232C43'
+    document.getElementsByClassName('screen')[0].style.background = '#182034'
     document.getElementsByClassName('button__container')[0].style.background =
-      'hsl(224, 36%, 15%)'
+      '#182034'
 
     knob = document.querySelector('.slider')
     knob.classList.add('theme1')
-    console.log(knob)
-    var button = document.querySelectorAll('.btn')
+    knob.classList.remove('theme2', 'theme3')
 
     for (i = 0; i < button.length; i++) {
-      button[i].style.boxShadow = '0 5px hsl(28, 16%, 65%)'
-      button[i].style.backgroundColor = '#hsl(30, 25%, 89%)'
-      button[i].style.color = 'hsl(221, 14%, 31%)'
+      button[i].style.boxShadow = '0 5px	#B4A597'
+      button[i].style.backgroundColor = '#EAE3DC'
+      button[i].style.color = '#444B5A'
     }
 
-    document.querySelector('.btn__del').style.backgroundColor =
-      'hsl(225, 21%, 49%)'
-    document.querySelector('.btn__del').style.boxShadow =
-      '0 5px hsl(224, 28%, 35%)'
+    document.querySelector('.btn__del').style.backgroundColor = '#637097'
+    document.querySelector('.btn__del').style.boxShadow = '0 5px #404E72'
     document.querySelector('.btn__del').style.color = 'white'
-    document.querySelector('.btn__reset').style.backgroundColor =
-      'hsl(225, 21%, 49%)'
-    document.querySelector('.btn__reset').style.boxShadow =
-      '0 5px hsl(224, 28%, 35%)'
+    document.querySelector('.btn__reset').style.backgroundColor = '#637097'
+    document.querySelector('.btn__reset').style.boxShadow = '0 5px #404E72'
     document.querySelector('.btn__reset').style.color = 'white'
-    document.querySelector('.btn__equal').style.backgroundColor =
-      ' hsl(6, 63%, 50%)'
-    document.querySelector('.btn__equal').style.boxShadow =
-      ' 0 5px hsl(6, 70%, 34%)'
+    document.querySelector('.btn__equal').style.backgroundColor = '#D03F2F'
+    document.querySelector('.btn__equal').style.boxShadow = ' 0 5px #93261A'
+    val = ''
   } else if (value == 2) {
-    document.body.style.backgroundColor = 'hsl(0, 0%, 90%)'
+    document.body.style.backgroundColor = '#E6E6E6'
     document.querySelector('.calculator__header').style.color = '#2D2D25'
     document.querySelector('.theme__title ').style.color = '#2D2D25'
-    document.querySelector('.slide__options').style.color = 'hsl(60, 10%, 19%)'
-    document.querySelector('.slider').style.background = 'hsl(0, 5%, 81%)'
-    document.querySelector('.screen').style.background = 'hsl(0, 0%, 93%)'
-    document.querySelector('.button__container').style.background =
-      'hsl(0, 5%, 81%)'
+    document.querySelector('.slide__options').style.color = '#35352C'
+    document.querySelector('.slider').style.background = '#D1CCCC'
+    document.querySelector('.screen').style.background = '#EDEDED'
+    document.querySelector('.button__container').style.background = '#D1CCCC'
 
     knob = document.querySelector('.slider')
     knob.classList.add('theme2')
-    console.log(knob)
-    var button = document.querySelectorAll('.btn')
+    knob.classList.remove('theme1', 'theme3')
 
     for (i = 0; i < button.length; i++) {
-      button[i].style.boxShadow = '0 5px  hsl(35, 11%, 61%)'
-      button[i].style.backgroundColor = 'hsl(45, 7%, 89%)'
-      button[i].style.color = 'hsl(60, 10%, 19%)'
+      button[i].style.boxShadow = '0 5px  #A69D91'
+      button[i].style.backgroundColor = '#E5E4E1'
+      button[i].style.color = '#35352C'
     }
 
-    document.querySelector('.btn__del').style.backgroundColor =
-      'hsl(185, 42%, 37%)'
-    document.querySelector('.btn__del').style.boxShadow =
-      '0 5px hsl(185, 58%, 25%)'
+    document.querySelector('.btn__del').style.backgroundColor = '#377F86'
+    document.querySelector('.btn__del').style.boxShadow = '0 5px #1B5F65'
     document.querySelector('.btn__del').style.color = 'white'
-    document.querySelector('.btn__reset').style.backgroundColor =
-      'hsl(185, 42%, 37%)'
-    document.querySelector('.btn__reset').style.boxShadow =
-      '0 5px hsl(185, 58%, 25%)'
+    document.querySelector('.btn__reset').style.backgroundColor = '#377F86'
+    document.querySelector('.btn__reset').style.boxShadow = '0 5px #1B5F65 '
     document.querySelector('.btn__reset').style.color = 'white'
-    document.querySelector('.btn__equal').style.backgroundColor =
-      'hsl(25, 98%, 40%)'
-    document.querySelector('.btn__equal').style.boxShadow =
-      ' 0 5px hsl(25, 99%, 27%)'
+    document.querySelector('.btn__equal').style.backgroundColor = '#CA5502'
+    document.querySelector('.btn__equal').style.boxShadow = ' 0 5px #893901'
+    val = ''
   } else if (value == 3) {
-    document.body.style.backgroundColor = 'hsl(268, 75%, 9%)'
-    document.querySelector('.calculator__header').style.color =
-      'hsl(52, 100%, 62%)'
-    document.querySelector('.theme__title ').style.color = 'hsl(52, 100%, 62%)'
-    document.querySelector('.slide__options').style.color = 'hsl(52, 100%, 62%)'
-    document.querySelector('.slider').style.background = 'hsl(268, 71%, 12%)'
-    document.querySelector('.screen').style.background = 'hsl(268, 71%, 12%)'
-    document.querySelector('.button__container').style.background =
-      'hsl(268, 71%, 12%)'
+    document.body.style.backgroundColor = '#160628'
+    document.querySelector('.calculator__header').style.color = '#FFE53D'
+    document.querySelector('.theme__title ').style.color = '#FFE53D'
+    document.querySelector('.slide__options').style.color = '#FFE53D'
+    document.querySelector('.slider').style.background = '#1D0934'
+    document.querySelector('.screen').style.background = '#1D0934'
+    document.querySelector('.button__container').style.background = '#1D0934'
 
     knob = document.querySelector('.slider')
     knob.classList.add('theme3')
-    console.log(knob)
-    var button = document.querySelectorAll('.btn')
 
     for (i = 0; i < button.length; i++) {
-      button[i].style.boxShadow = '0 5px hsl(290, 70%, 36%)'
-      button[i].style.backgroundColor = 'hsl(268, 47%, 21%)'
-      button[i].style.color = 'hsl(52, 100%, 62%)'
+      button[i].style.boxShadow = '0 5px #871C9C'
+      button[i].style.backgroundColor = '#341C4F'
+      button[i].style.color = '#FFE53D'
     }
 
-    document.querySelector('.btn__del').style.backgroundColor =
-      'hsl(281, 89%, 26%)'
-    document.querySelector('.btn__del').style.boxShadow =
-      '0 5px  hsl(285, 91%, 52%)'
+    document.querySelector('.btn__del').style.backgroundColor = '#58077D'
+    document.querySelector('.btn__del').style.boxShadow = '0 5px  #BC15F4'
     document.querySelector('.btn__del').style.color = 'white'
-    document.querySelector('.btn__reset').style.backgroundColor =
-      'hsl(281, 89%, 26%)'
-    document.querySelector('.btn__reset').style.boxShadow =
-      '0 5px  hsl(285, 91%, 52%)'
+    document.querySelector('.btn__reset').style.backgroundColor = '#58077D'
+    document.querySelector('.btn__reset').style.boxShadow = '0 5px  #BC15F4'
     document.querySelector('.btn__reset').style.color = 'white'
-    document.querySelector('.btn__equal').style.backgroundColor =
-      'hsl(176, 100%, 44%)'
-    document.querySelector('.btn__equal').style.boxShadow =
-      ' 0 5px hsl(177, 92%, 70%)'
-    document.querySelector('.btn__equal').style.color = 'hsl(198, 20%, 13%)'
+    document.querySelector('.btn__equal').style.backgroundColor = '#00E0D1'
+    document.querySelector('.btn__equal').style.boxShadow = ' 0 5px #6CF9F2'
+    document.querySelector('.btn__equal').style.color = '#1B2428'
+    val = ''
   }
 }
+
+const calculator = document.querySelector('.calculator__container')
+const keys = calculator.querySelector('.button__container')
+const display = document.querySelector('.screen')
+
+keys.addEventListener('click', (e) => {
+  if (e.target.matches('button')) {
+    const key = e.target
+    const action = key.dataset.action
+    const keyContent = key.textContent
+    const displayedNum = display.textContent
+    if (!action) {
+      if (displayedNum === '0') {
+        display.textContent = keyContent
+      } else {
+        display.textContent = displayedNum + keyContent
+      }
+      console.log(display.textContent)
+    }
+    if (
+      action === 'add' ||
+      action === 'subtract' ||
+      action === 'multiply' ||
+      action === 'divide'
+    ) {
+      console.log('operator key!')
+    }
+    if (action === 'decimal') {
+      display.textContent = displayedNum + '.'
+    }
+
+    if (action === 'clear') {
+      console.log('clear key!')
+    }
+
+    if (action === 'equal') {
+      console.log('equal key!')
+    }
+    if (action === 'delete') {
+      console.log('delete key!')
+    }
+  }
+})
